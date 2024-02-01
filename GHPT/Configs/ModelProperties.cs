1673c042c6b9608ceb15adb90249b355e7969153
Sam Gregson <samgregson@hotmail.com>
2024-02-01 15:20:01 +0000

    Fix serialisation of saved configs

---
+++ GHPT/Configs/ModelProperties.cs
@@ -0,0 +1,14 @@
+﻿namespace GHPT.Configs
+{
+    public class ModelProperties
+    {
+        public ModelIcon Icon { get; }
+        public string Url { get; }
+
+        public ModelProperties(ModelIcon icon, string url)
+        {
+            Icon = icon;
+            Url = url;
+        }
+    }
+}
--- GHPT/Configs/Models.cs
+++ GHPT/Configs/Models.cs
@@ -4,12 +4,15 @@
     {
         public static Dictionary<string, GPTVersion> ModelOptions = new()
         {
-            { "gpt-4", GPTVersion.GPT4 },
-            { "gpt-3.5-turbo", GPTVersion.GPT3_5 },
-            { "gpt-3.5-turbo-0301", GPTVersion.GPT3_5 },
-            { "gpt-3.5-turbo-0613", GPTVersion.GPT3_5 },
-            { "gpt-3.5-turbo-16k", GPTVersion.GPT3_5 },
-            { "gpt-3.5-turbo-16k-0613", GPTVersion.GPT3_5 }
+            { "gpt-4", new ModelProperties(ModelIcon.GPT4, openAiUrl)},
+            { "gpt-3.5-turbo",  new ModelProperties(ModelIcon.GPT3_5, openAiUrl) },
+            { "gpt-3.5-turbo-0301", new ModelProperties(ModelIcon.GPT3_5, openAiUrl) },
+            { "gpt-3.5-turbo-0613",  new ModelProperties(ModelIcon.GPT3_5, openAiUrl) },
+            { "gpt-3.5-turbo-16k",  new ModelProperties(ModelIcon.GPT3_5, openAiUrl) },
+            { "gpt-3.5-turbo-16k-0613", new ModelProperties(ModelIcon.GPT3_5, openAiUrl) },
+            { "mistralai/Mixtral-8x7B-Instruct-v0.1", new ModelProperties(ModelIcon.None, togetherAiUrl) },
+            { "togethercomputer/llama-2-70b-chat", new ModelProperties(ModelIcon.None, togetherAiUrl) },
+            { "togethercomputer/CodeLlama-34b-Instruct", new ModelProperties(ModelIcon.None, togetherAiUrl) }
         };
     }
 }
